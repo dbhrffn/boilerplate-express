5152013560f01317b767c873f548856b9b892d93
dbhrffn <dibahariffin@gmail.com>
2023-05-21 02:00:38 +0000

    serve static assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+//declare public
+// app.use(express.static(__dirname + "/public"));
+
+// use files in public
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
