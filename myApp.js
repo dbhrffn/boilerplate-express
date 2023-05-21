9e9998a9edaf04e176aefe6b676c4d5d38e048e2
dbhrffn <dibahariffin@gmail.com>
2023-05-21 02:04:50 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -11,6 +11,10 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"});
+});
+
 
 
 
